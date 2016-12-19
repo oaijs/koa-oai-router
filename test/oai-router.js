cdd0fba2697de11adfed29b05cf2974b87bcb02f
bitebit <kfbygj@gmail.com>
2016-12-19 23:55:47 +0800

    add enableParser options, as internal option.

--- test/oai-router.js
+++ test/oai-router.js
@@ -114,3 +114,67 @@ test('with no validator option', t => {
   app.use(router.apiExplorer());
   t.pass();
 })
+
+test.cb('with enableParser false option', t => {
+  const app = new Koa();
+  const server = app.listen();
+
+  const opt = {
+    apiDoc: `${__dirname}/fixtures/api/api.json`,
+    controllerDir: `${__dirname}/fixtures/controller`,
+    versioning: true,
+    enableParser: false
+  };
+
+  const router = new Router(opt);
+  router.init(require(`${__dirname}/fixtures/api/api.json`));
+
+  app.use(bodyParser());
+  app.use(router.routes());
+
+  request(server)
+    .get('/v1/api/pets')
+    .query({limit: 10, page: 1})
+    .expect(200)
+    .end(function(err, res) {
+      if (err) {
+        console.error(err, res);
+        throw err;
+      }
+      t.end();
+    });
+})
+
+test.cb('with handler function option', t => {
+  const api = require(`${__dirname}/fixtures/api/api.json`);
+  const app = new Koa();
+  const server = app.listen();
+
+  const opt = {
+    apiDoc: `${__dirname}/fixtures/api/api.json`,
+    controllerDir: `${__dirname}/fixtures/controller`,
+    versioning: true,
+    enableParser: false
+  };
+
+  const router = new Router(opt);
+  api.paths['/pets'].get['x-oai-controller'] = [{
+    handler: (ctx)=> {
+      ctx.body = 'handler-function';
+    }
+  }]
+  router.init(api);
+
+  app.use(bodyParser());
+  app.use(router.routes());
+
+  request(server)
+    .get('/v1/api/pets')
+    .query({limit: 10, page: 1})
+    .expect(200)
+    .end(function(err, res) {
+      if (err) throw err;
+      t.is(res.text, 'handler-function');
+      t.end();
+    });
+})
